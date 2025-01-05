121ee7c5442cda967720cd93380c3a84210c3eff
JoseQuintas2021 <jose.nuno.quintas@gmail.com>
2025-01-05 20:41:47 +0000

    AGUI and MenuViewer tests

--- src/test/java/timelessodyssey/gui/LanternaGUITest.java
+++ src/test/java/timelessodyssey/gui/LanternaGUITest.java
@@ -1,24 +1,249 @@
 package timelessodyssey.gui;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.*;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyListener;
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.net.URISyntaxException;
 import java.awt.event.KeyEvent;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.spy;
+import static org.junit.Assert.assertNull;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 public class LanternaGUITest {
+    @Mock
+    private KeyEvent keyEvent;
+
     private LanternaGUI lanternaGUI;
 
     @BeforeEach
-    public void setUp() throws IOException, URISyntaxException, FontFormatException {
-        lanternaGUI = spy(new LanternaGUI(mock(ScreenCreator.class), "test"));
+    public void setUp() throws IOException, URISyntaxException, FontFormatException, NoSuchFieldException, IllegalAccessException {
+        ScreenCreator screenCreator = spy(new LanternaScreenCreator(
+            new DefaultTerminalFactory(),
+            new TerminalSize(80, 24),
+            new Rectangle(0, 0, 800, 600)
+        ));
+
+        lanternaGUI = spy(new LanternaGUI(screenCreator, "test"));
+    }
+
+    @Test
+    public void testGetNextActionNONE() throws NoSuchFieldException, IllegalAccessException {
+        setKeyPressed(null);
+
+        assertEquals(GUI.Action.NONE, lanternaGUI.getNextAction());
+    }
+
+    @Test
+    public void testGetNextActionLEFT() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_LEFT).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.LEFT, action);
+    }
+
+    @Test
+    public void testGetNextActionRIGHT() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_RIGHT).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.RIGHT, action);
+    }
+
+    @Test
+    public void testGetNextActionUP() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_UP).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.UP, action);
+    }
+
+    @Test
+    public void testGetNextActionDOWN() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_DOWN).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.DOWN, action);
+    }
+
+    @Test
+    public void testGetNextActionQUIT() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_ESCAPE).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.QUIT, action);
+    }
+
+    @Test
+    public void testGetNextActionSELECT() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_ENTER).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.SELECT, action);
+    }
+
+    @Test
+    public void testGetNextActionJUMP() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_SPACE).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.JUMP, action);
+    }
+
+    @Test
+    public void testGetNextActionDASH() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_X).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.DASH, action);
+    }
+
+    @Test
+    public void testGetNextActionDEFAULT() throws NoSuchFieldException, IllegalAccessException {
+        doReturn(KeyEvent.VK_D).when(keyEvent).getKeyCode();
+        setKeyPressed(keyEvent);
+
+        GUI.Action action = lanternaGUI.getNextAction();
+
+        assertEquals(GUI.Action.NONE, action);
+    }
+
+    @Test
+    public void testKeyAdapterPRESSED_INVALIDKEY() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        lanternaGUI.setKeySpam(true);
+
+        KeyAdapter keyAdapter = createKeyAdapter();
+
+        when(keyEvent.getKeyCode()).thenReturn(KeyEvent.VK_0);
+
+
+        keyAdapter.keyPressed(keyEvent);
+
+
+        KeyEvent actualKeyPressed = getKeyPressed();
+
+        assertEquals(keyEvent, actualKeyPressed);
+    }
+
+    @Test
+    public void testKeyAdapterPRESSED_NO_KEYSPAM() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        KeyAdapter keyAdapter = createKeyAdapter();
+
+
+        keyAdapter.keyPressed(keyEvent);
+
+
+        KeyEvent actualKeyPressed = getKeyPressed();
+
+        assertEquals(keyEvent, actualKeyPressed);
+    }
+
+    @Test
+    public void testKeyAdapterPRESSED_KEYSPAM() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        lanternaGUI.setKeySpam(true);
+
+        KeyAdapter keyAdapter = createKeyAdapter();
+
+        when(keyEvent.getKeyCode()).thenReturn(KeyEvent.VK_LEFT);
+
+
+        keyAdapter.keyPressed(keyEvent);
+
+
+        KeyEvent actualKeyPressed = getKeyPressed();
+        KeyEvent actualPriorityKeyPressed = getPriorityKeyPressed();
+
+        assertEquals(keyEvent, actualKeyPressed);
+        assertEquals(keyEvent, actualPriorityKeyPressed);
+    }
+
+    @Test
+    public void testKeyAdapterRELEASED_INVALIDKEY() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        lanternaGUI.setKeySpam(true);
+
+        KeyAdapter keyAdapter = createKeyAdapter();
+
+        when(keyEvent.getKeyCode()).thenReturn(KeyEvent.VK_0);
+
+
+        keyAdapter.keyReleased(keyEvent);
+
+
+        KeyEvent actualKeyPressed = getKeyPressed();
+        KeyEvent actualPriorityKeyPressed = getPriorityKeyPressed();
+
+        assertEquals(actualPriorityKeyPressed, actualKeyPressed);
+    }
+
+    @Test
+    public void testKeyAdapterRELEASED_NO_KEYSPAM() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        KeyAdapter keyAdapter = createKeyAdapter();
+
+
+        keyAdapter.keyReleased(keyEvent);
+
+
+        KeyEvent actualKeyPressed = getKeyPressed();
+        KeyEvent actualPriorityKeyPressed = getPriorityKeyPressed();
+
+        assertEquals(actualPriorityKeyPressed, actualKeyPressed);
+    }
+
+    @Test
+    public void testKeyAdapterRELEASED_KEYSPAM() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException, NoSuchFieldException {
+        lanternaGUI.setKeySpam(true);
+
+        KeyAdapter keyAdapter = createKeyAdapter();
+
+        when(keyEvent.getKeyCode()).thenReturn(KeyEvent.VK_LEFT);
+
+
+        keyAdapter.keyReleased(keyEvent);
+
+
+        KeyEvent actualKeyPressed = getKeyPressed();
+        KeyEvent actualPriorityKeyPressed = getPriorityKeyPressed();
+
+        assertNull(actualKeyPressed);
+        assertNull(actualPriorityKeyPressed);
     }
 
     @Test
@@ -30,12 +255,44 @@ public class LanternaGUITest {
 
         lanternaGUI.setKeySpam(true);
 
-        assertEquals(true, getPriorityKeyPressed());
+        assertEquals(true, getKeySpam());
+    }
+
+    public void setPriorityKeyPressed(KeyEvent keyEvent) throws NoSuchFieldException, IllegalAccessException {
+        Field keyField = LanternaGUI.class.getDeclaredField("priorityKeyPressed");
+        keyField.setAccessible(true);
+        keyField.set(lanternaGUI, keyEvent);
     }
 
     public KeyEvent getPriorityKeyPressed() throws NoSuchFieldException, IllegalAccessException {
         Field keyField = LanternaGUI.class.getDeclaredField("priorityKeyPressed");
         keyField.setAccessible(true);
         return (KeyEvent) keyField.get(lanternaGUI);
+    }
+
+
+    public void setKeyPressed(KeyEvent keyEvent) throws NoSuchFieldException, IllegalAccessException {
+        Field keyField = LanternaGUI.class.getDeclaredField("keyPressed");
+        keyField.setAccessible(true);
+        keyField.set(lanternaGUI, keyEvent);
+    }
+
+    public KeyEvent getKeyPressed() throws NoSuchFieldException, IllegalAccessException {
+        Field keyField = LanternaGUI.class.getDeclaredField("keyPressed");
+        keyField.setAccessible(true);
+        return (KeyEvent) keyField.get(lanternaGUI);
+    }
+
+
+    public boolean getKeySpam() throws NoSuchFieldException, IllegalAccessException {
+        Field keySpamField = LanternaGUI.class.getDeclaredField("keySpam");
+        keySpamField.setAccessible(true);
+        return (boolean) keySpamField.get(lanternaGUI);
+    }
+
+    public KeyAdapter createKeyAdapter() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method createKeyAdapterMethod = LanternaGUI.class.getDeclaredMethod("createKeyAdapter");
+        createKeyAdapterMethod.setAccessible(true);
+        return (KeyAdapter) createKeyAdapterMethod.invoke(lanternaGUI);
     }
 }
---
+++ src/test/java/timelessodyssey/view/screens/MenuViewerTest.java
@@ -0,0 +1,91 @@
+package timelessodyssey.view.screens;
+
+import com.googlecode.lanterna.TextColor;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import timelessodyssey.gui.ResizableGUI;
+import timelessodyssey.model.menu.Entry;
+import timelessodyssey.model.menu.Menu;
+import timelessodyssey.view.Sprite;
+import timelessodyssey.view.SpriteLoader;
+import timelessodyssey.view.ViewerProvider;
+import timelessodyssey.view.menu.EntryViewer;
+import timelessodyssey.view.text.TextViewer;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.net.URISyntaxException;
+import java.util.List;
+
+import static org.mockito.Mockito.*;
+
+public class MenuViewerTest {
+    private MenuViewer menuViewer;
+    private EntryViewer entryViewer;
+    private Menu menu;
+    private ViewerProvider viewerProvider;
+    private SpriteLoader spriteLoader;
+
+    @BeforeEach
+    public void setUp() throws IOException, URISyntaxException {
+        menu = spy(new Menu() {
+            @Override
+            protected List<Entry> createEntries() {
+                return List.of(
+                    new Entry(1, 1, Entry.Type.START_GAME),
+                    new Entry(1, 2, Entry.Type.SETTINGS),
+                    new Entry(1, 3, Entry.Type.EXIT),
+                    new Entry(1, 4, Entry.Type.RESOLUTION),
+                    new Entry(1, 5, Entry.Type.TO_MAIN_MENU)
+                );
+            }
+        });
+
+        spriteLoader = spriteFilepath -> mock(Sprite.class);
+
+        entryViewer = mock(EntryViewer.class);
+
+        viewerProvider = spy(new ViewerProvider(spriteLoader));
+        when(viewerProvider.getEntryViewer()).thenReturn(entryViewer);
+
+        menuViewer = new MenuViewer(menu, viewerProvider);
+    }
+
+    @Test
+    public void testDrawEntriesEMPTY_LIST() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        ResizableGUI gui = mock(ResizableGUI.class);
+        List<Entry> entries = List.of();
+
+        invokeDrawEntries(gui, entries);
+
+        verify(entryViewer, never()).draw(any(), any(), any());
+    }
+
+    @Test
+    public void testDrawEntriesFIRST_POSITION() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        ResizableGUI gui = mock(ResizableGUI.class);
+        List<Entry> entries = menu.getEntries();
+
+        invokeDrawEntries(gui, entries);
+
+        verify(entryViewer, times(entries.size())).draw(any(), any(), any());
+    }
+
+    @Test
+    public void testDrawEntriesSECOND_POSITION() throws InvocationTargetException, NoSuchMethodException, IllegalAccessException {
+        ResizableGUI gui = mock(ResizableGUI.class);
+        List<Entry> entries = menu.getEntries();
+        menu.moveDown();
+
+        invokeDrawEntries(gui, entries);
+
+        verify(entryViewer, times(entries.size())).draw(any(), any(), any());
+    }
+
+    public void invokeDrawEntries(ResizableGUI gui, List<Entry> entries) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method drawEntriesMethod = MenuViewer.class.getDeclaredMethod("drawEntries", ResizableGUI.class, List.class);
+        drawEntriesMethod.setAccessible(true);
+        drawEntriesMethod.invoke(menuViewer, gui, entries);
+    }
+}
